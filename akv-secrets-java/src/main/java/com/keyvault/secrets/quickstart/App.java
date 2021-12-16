0d9eec4c1b6c26b0d7354083cde5ce1c5cf2a99c
VKM2214 <vigneshmurugankumar@gmail.com>
2021-12-16 15:55:25 +0100

    Update App.java

--- akv-secrets-java/src/main/java/com/keyvault/secrets/quickstart/App.java
+++ akv-secrets-java/src/main/java/com/keyvault/secrets/quickstart/App.java
@@ -11,43 +11,12 @@ import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
 
 public class App {
     public static void main(String[] args) throws InterruptedException, IllegalArgumentException {
-        String keyVaultName = System.getenv("KEY_VAULT_NAME");
-        String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
-
-        System.out.printf("key vault name = %s and key vault URI = %s \n", keyVaultName, keyVaultUri);
-
-        SecretClient secretClient = new SecretClientBuilder()
-            .vaultUrl(keyVaultUri)
-            .credential(new DefaultAzureCredentialBuilder().build())
-            .buildClient();
-
-        Console con = System.console();  
-
-        String secretName = "mySecret";
-
-        System.out.println("Please provide the value of your secret > ");
-        
-        String secretValue = con.readLine();
-
-        System.out.print("Creating a secret in " + keyVaultName + " called '" + secretName + "' with value '" + secretValue + "` ... ");
-
-        secretClient.setSecret(new KeyVaultSecret(secretName, secretValue));
-
-        System.out.println("done.");
-        System.out.println("Forgetting your secret.");
-        
-        secretValue = "";
-        System.out.println("Your secret's value is '" + secretValue + "'.");
-
-        System.out.println("Retrieving your secret from " + keyVaultName + ".");
-
-        KeyVaultSecret retrievedSecret = secretClient.getSecret(secretName);
+        String keyVaultUri = "https://readsecretvault.vault.azure.net/";
+        String myurl = "StorrageAccontName";
+        System.out.printf("key vault URI = %s \n", keyVaultUri);
+        KeyVaultSecret retrievedSecret = secretClient.getSecret(myurl);
 
         System.out.println("Your secret's value is '" + retrievedSecret.getValue() + "'.");
-        System.out.print("Deleting your secret from " + keyVaultName + " ... ");
-
-        SyncPoller<DeletedSecret, Void> deletionPoller = secretClient.beginDeleteSecret(secretName);
-        deletionPoller.waitForCompletion();
 
         System.out.println("done.");
     }
